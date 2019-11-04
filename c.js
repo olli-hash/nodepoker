ac3b0931d5bf26bf18fae61f945ae3404e096f8f
Oliver_localgit <oliver@gedankenfenster.de>
2019-11-04 09:45:30 +0100

    fake4

--- c.js
+++ c.js
@@ -8,3 +8,24 @@ app.get("some_nice", somefunc)
 app.listen(port, func2)
 
 
+
+/*          Git & GitHub
+
+
+    Wieso habe ich Dich als "contributor" eingetragen, wenn Du am Ende doch nur in Deinem eigenen Branch arbeitest?
+    
+    Das Forken kann bei öffentlichen GitHub-Projekten doch so oder so jeder…
+    
+    (Siehe: https://help.github.com/en/github/getting-started-with-github/fork-a-repo)
+    
+    
+
+
+    
+
+
+
+
+*/
+
+
