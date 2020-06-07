4f156c6d098daa2d893521fde1c6a33d2707c44f
olli-hash <52583865+olli-hash@users.noreply.github.com>
2020-06-07 17:24:29 +0200

    Update pokerjs.js

--- pokerjs.js
+++ pokerjs.js
@@ -33,29 +33,60 @@
 
 
 
+/*
+	create game (single table mode | multi table mode)
+	add cardstacks to game
+	add users to game/table
+	start game
 
-
+*/
 
 
 
 
 // ++++++++++++++++++++++++++++++++++++++++++++++++
-											exports.express_number_class = express_number_class
+											exports.game_class = game_class
 // ++++++++++++++++++++++++++++++++++++++++++++++++
 
 
-function express_number_class (a,b) {
+function game_class (users) {
+	
+	this.users = users
+	
+}
+
+game_class.prototype.add_user (u) {
+	
+	this.users.push(u)
+	
+	
+	
+}
+
+
+
+
+
+// -------- -------- -------- -------- -------- -------- -------- -------- -------- -------- 
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++
+											exports.two_digits_number_class = two_digits_number_class
+// ++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+function two_digits_number_class (a,b) {
 	
 	this.base = b
 	this.counter = a
 	this.value = a / b
 }
 
-express_number_class.prototype.change = function (a,b) {
+two_digits_number_class.prototype.change = function (a,b) {
 	
-	//express_number_class.prototype.constructor(a,b)    ??
+	//two_digits_number_class.prototype.constructor(a,b)    ??
 	
-	express_number_class.prototype.constructor.call(this,a,b)
+	two_digits_number_class.prototype.constructor.call(this,a,b)
 	
 }
 
@@ -64,7 +95,7 @@ express_number_class.prototype.change = function (a,b) {
 
 
 // ++++++++++++++++++++++++++++++++++++++++++++++++
-exports.give_card_from_cardstack = give_card_from_cardstack
+											exports.give_card_from_cardstack = give_card_from_cardstack
 // ++++++++++++++++++++++++++++++++++++++++++++++++
 
 function give_card_from_cardstack(cardstack) {
@@ -78,7 +109,7 @@ function give_card_from_cardstack(cardstack) {
 
 
 // ++++++++++++++++++++++++++++++++++++++++++++++++
-exports.give_card = give_card
+											exports.give_card = give_card
 // ++++++++++++++++++++++++++++++++++++++++++++++++
 
 function give_card() {
@@ -95,7 +126,7 @@ function give_card() {
 
 
 // ++++++++++++++++++++++++++++++++++++++++++++++++
-exports.eval_family = eval_family
+											exports.eval_family = eval_family
 // ++++++++++++++++++++++++++++++++++++++++++++++++
 
 function eval_family() {
@@ -110,11 +141,16 @@ function eval_family() {
 
 // -------- -------- -------- -------- -------- -------- -------- -------- -------- -------- 
 
-
+// ++++++++++++++++++++++++++++++++++++++++++++++++
+											exports.cardstack_class = cardstack_class
+// ++++++++++++++++++++++++++++++++++++++++++++++++
 
 function cardstack_class() {
 	
+// Für eine Konstruktorfunktion ist das zu initialisierende Objekt lediglich ein Objekt, das auf
+// Bearbeitung wartet.
 
+// Eine Unterfunktion wird vergessen, wenn die Konstruktorfunktion stirbt. ???
 	
 	function give_card() {
 		
